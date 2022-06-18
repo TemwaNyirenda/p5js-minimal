e2470e0420ba69813c88f0a4426c147a19584d54
TemwaNyirenda <temwanyirenda00@gmail.com>
2022-06-18 14:23:19 +0200

    Corrected array again

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //An array of nice color codes we shall pick from
-let niceColors = ["#C5514C", "#EC9D6C", "#FAE2B7", "#6D7D8F", "#52728C, #FFFFFF, #FFC0CB"];
+let niceColors = ["#C5514C", "#EC9D6C", "#FAE2B7", "#6D7D8F", "#52728C", "#000", "#FFC0CB"];
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
